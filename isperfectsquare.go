864a6d09e4f2a22d1063eef9c88a307026470b6a
Saumya Pathak <155012097+saumya712@users.noreply.github.com>
2026-02-10 23:36:20 +0530

    Implement function to check perfect squares

---
+++ isperfectsquare.go
@@ -0,0 +1,18 @@
+func isPerfectSquare(num int) bool {
+	left := 1
+	right := num
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if mid*mid == num {
+			return true
+		} else if mid*mid < num {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return false
+}
